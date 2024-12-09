d80b572aff0e5ea38dcfe17c05daa6d40ed7aea4
Samanta <samantarukmanegmail.com>
2024-12-09 11:01:13 +0200

    Initial commit: Added existing files

---
+++ spele_ed_need/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace spele_ed_need
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(15, 15);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(423, 15);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(510, 556);
+            this.panel2.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(24, 340);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(56, 25);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Time";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(24, 315);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(137, 25);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Fallen Objects";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 290);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(178, 25);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Wrong Right Clicks";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(166, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Wrong Left Clicks";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Correct Right Clicks";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Correct Left Clicks";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(92, 32);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 30);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 35);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Level:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Score";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 128);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 72);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 585);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ spele_ed_need/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace spele_ed_need
+{
+    public partial class Form1 : Form
+    {
+
+
+        private int L = 30;
+        private int Lxp = 400, Lyp = 400;
+        private int score = 0;
+        private Random R = new Random();
+        private int objectCount = 10;
+        private int currentLevel = 1;
+        private int objectSpeed = 5;
+        private bool levelUpTriggered = false;
+
+        private int correctLeftClickCount = 0;
+        private int correctRightClickCount = 0;
+        private int wrongLeftClickCount = 0;
+        private int wrongRightClickCount = 0;
+        private int fallenObjectCount = 0;
+        private int gameTime = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Width = Lxp;
+            panel1.Height = Lyp;
+            panel2.Left = Lxp + 30;
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+
+            MessageBox.Show(
+                "Instructions:\n" +
+                "1. Click on the edible items with the left mouse button.\n" +
+                "2. Right-click on the inedible items.\n" +
+                "3. Points:\n" + 
+                "   +10 for a correct click.\n" + 
+                "   -10 for a mistake.\n" +
+                "4. Level up:\n" + "   Reach 50 points to advance to the next level.\n" +
+                "5. Game ends:\n" + "   After 30 seconds or if your score falls below -50.\n" +
+                "Final Score = Base Score + (Correct Clicks x Level x 2) - (Mistakes x 5).\n" +
+                "Press 'Start' to begin the game!",
+                "Game Instructions",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            score = 0;
+
+            correctLeftClickCount = 0;
+            correctRightClickCount = 0;
+            wrongLeftClickCount = 0;
+            wrongRightClickCount = 0;
+            fallenObjectCount = 0;
+            gameTime = 0;
+
+            UpdateStatistics();
+
+            int level = (int)numericUpDown1.Value;
+
+            switch (level)
+            {
+                case 1:
+                    objectCount = 10;
+                    break;
+                case 2:
+                    objectCount = 15;
+                    break;
+                case 3:
+                    objectCount = 20;
+                    break;
+                default:
+                    objectCount = 10;
+                    break;
+            }
+
+            for (int i = 0; i < objectCount; i++)
+            {
+                CreateFallingObject();
+            }
+
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+        }
+
+
+        private void CreateFallingObject()
+        {
+            PictureBox obj = new PictureBox
+            {
+                Width = L * 13 / 10,
+                Height = L * 13 / 10,
+                Left = R.Next(0, Lxp - L),
+                Top = R.Next(-Lyp, 0),
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+
+            int r = R.Next(0, 100);
+
+            if (r < 50)
+            {
+                obj.ImageLocation = @"Resources\f" + R.Next(1, 6) + ".png";
+                obj.Tag = "Y";
+            }
+            else
+            {
+                obj.ImageLocation = @"Resources\a" + R.Next(1, 6) + ".png";
+                obj.Tag = "N";
+            }
+
+            obj.MouseDown += new MouseEventHandler(p_MouseDown);
+            panel1.Controls.Add(obj);
+        }
+
+
+        private void p_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (sender is PictureBox s)
+            {
+                if (e.Button == MouseButtons.Left && s.Tag.ToString() == "Y")
+                {
+                    score += 10;
+                    correctLeftClickCount++;
+                    PlaySound("correct_click.wav");
+                    s.BackColor = Color.Green;
+                }
+                else if (e.Button == MouseButtons.Right && s.Tag.ToString() == "N")
+                {
+                    score += 10;
+                    correctRightClickCount++;
+                    PlaySound("correct_click.wav");
+                    s.BackColor = Color.Blue;
+                }
+                else
+                {
+                    score -= 10;
+                    if (e.Button == MouseButtons.Left) wrongLeftClickCount++;
+                    else if (e.Button == MouseButtons.Right) wrongRightClickCount++;
+                    PlaySound("wrong_click.wav");
+                    s.BackColor = Color.Red;
+                }
+
+                Timer disappearTimer = new Timer
+                {
+                    Interval = 200
+                };
+                disappearTimer.Tick += (o, args) =>
+                {
+                    disappearTimer.Stop();
+                    panel1.Controls.Remove(s);
+                };
+                disappearTimer.Start();
+
+                UpdateStatistics();
+            }
+        }
+
+
+        private void PlaySound(string fileName)
+        {
+            try
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(fileName);
+                player.Play();
+            }
+            catch
+            {
+                MessageBox.Show("Sound file not found: " + fileName);
+            }
+        }
+
+
+        private void UpdateStatistics()
+        {
+            label2.Text = $"Score: {score}"; 
+            label5.Text = $"Correct Left Clicks: {correctLeftClickCount}";
+            label6.Text = $"Correct Right Clicks: {correctRightClickCount}";
+            label7.Text = $"Wrong Left Clicks: {wrongLeftClickCount}";
+            label8.Text = $"Wrong Right Clicks: {wrongRightClickCount}";
+            label9.Text = $"Fallen Objects: {fallenObjectCount}";
+            label10.Text = $"Time: {gameTime}s";
+        }
+
+
+        private void CheckLevelProgress()
+        {
+            if (score >= 50 * currentLevel && currentLevel < 3 && !levelUpTriggered)
+            {
+                levelUpTriggered = true;
+
+                currentLevel++;
+                numericUpDown1.Value = currentLevel;
+
+                MessageBox.Show($"Level up! New level: {currentLevel}");
+
+                objectCount += 5;
+                objectSpeed += 2;
+
+                panel1.Controls.Clear();
+                for (int i = 0; i < objectCount; i++)
+                {
+                    CreateFallingObject();
+                }
+            }
+
+            if (score < 50 * currentLevel)
+            {
+                levelUpTriggered = false;
+            }
+        }
+
+
+        private void CalculateFinalScore()
+        {
+            double finalScore = score
+            + (correctLeftClickCount + correctRightClickCount) * (int)numericUpDown1.Value * 2
+            - (wrongLeftClickCount + wrongRightClickCount + fallenObjectCount) * 5;
+
+            MessageBox.Show($"Your final score: {finalScore:F2}");
+        }
+
+
+        private void EndGame()
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+
+            CalculateFinalScore();
+
+            MessageBox.Show("Game Over! Thanks for playing!");
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                MessageBox.Show("Author: Samanta Rukmane");
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            gameTime++;
+            UpdateStatistics();
+
+            CheckLevelProgress();
+
+            if (gameTime >= 30 || score < -50)
+            {
+                EndGame();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            foreach (Control c in panel1.Controls)
+            {
+                if (c is PictureBox obj)
+                {
+                    obj.Top += 5;
+
+                    if (obj.Top > Lyp)
+                    {
+                        fallenObjectCount++;
+                        score -= 10;
+
+                        UpdateStatistics();
+
+                        obj.Top = R.Next(-Lyp, 0);
+                        obj.Left = R.Next(0, Lxp - L);
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
